7086a99002902e9d2ca994acb7f1371da7903366
Teri <erk.nishimura@gmail.com>
2024-11-06 16:50:23 +0900

    refactor: fix path

--- frontend/app/components/layout/Header.tsx
+++ frontend/app/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Button } from '../ui/button/Button'
+import { Button } from '../ui/Button'
 
 export default function Header() {
   return (
