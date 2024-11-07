35af2512d236056af073bea9ca5dd4d43120eb17
Teri <erk.nishimura@gmail.com>
2024-11-07 13:59:19 +0900

    #25 add: Share Dialog and Drawer

---
+++ frontend/src/app/components/ui/dialogs/ShareDialog.tsx
@@ -0,0 +1,66 @@
+'use client'
+
+import Image from 'next/image'
+import { useState } from 'react'
+import * as React from 'react'
+import { FiShare } from 'react-icons/fi'
+import { IoMail } from 'react-icons/io5'
+import Line from '~/public/line.svg'
+import { Button } from '../buttons/Button'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './Dialog'
+
+export function ShareDialog() {
+  const [open, setOpen] = useState(false)
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="link">
+          <FiShare />
+          <span>シェアする</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[470px]">
+        <DialogHeader>
+          <DialogTitle className="mx-auto">
+            <Image
+              src="logo.svg"
+              alt="minnaka map"
+              width={230}
+              height={52}
+            />
+          </DialogTitle>
+          <DialogDescription className="text-center">
+            気になるお店をみんなにシェアしよう！
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid grid-cols-2 gap-3">
+          <Button variant="outline" className="flex items-center h-auto [&_svg]:size-6 py-3">
+            <Line
+              width={18}
+              height={18}
+              fill="none"
+              style={{ display: 'block' }}
+            />
+            <span>LINEでシェア</span>
+          </Button>
+          <Button variant="outline" className="flex items-center h-auto [&_svg]:size-6 py-3">
+            <IoMail
+              width={18}
+              height={18}
+              className="text-primary"
+            />
+            <span>メールでシェア</span>
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
