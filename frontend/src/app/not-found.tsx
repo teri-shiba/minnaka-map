219ea40b11041c75f9ee78543def974fde0c84df
Teri <erk.nishimura@gmail.com>
2024-11-10 18:11:44 +0900

    #26 add: 404 page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './components/ui/buttons/Button'
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center space-y-4 my-14">
+      <Image src="/image-404.png" alt="Not Found" width={240} height={200} />
+      <h1>Not Found</h1>
+      <p>お探しのページは見つかりませんでした。</p>
+      <Button asChild size="lg">
+        <Link href="/">トップページに戻る</Link>
+      </Button>
+    </div>
+  )
+}
