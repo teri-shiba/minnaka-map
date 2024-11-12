73230686b6649250b2f654e16fa51fd5b80151d9
Teri <erk.nishimura@gmail.com>
2024-11-12 14:06:39 +0900

    # 27 add: fetcher

--- frontend/src/app/lib/utils.ts
+++ frontend/src/app/lib/utils.ts
@@ -1,6 +1,18 @@
+import type { AxiosError, AxiosResponse } from 'axios'
+import axios from 'axios'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function fetcher(url: string) {
+  return axios
+    .get(url)
+    .then((res: AxiosResponse) => res.data)
+    .catch((err: AxiosError) => {
+      console.error(err.message)
+      throw err
+    })
 }
