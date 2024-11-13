86482c4f95c84dfdc62ed8606b725ec9f48ce7c1
Teri <erk.nishimura@gmail.com>
2024-11-13 17:25:58 +0900

    refactor: rubocop

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -40,7 +40,7 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.time_zone = 'Tokyo'
+    config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
   end
 end
