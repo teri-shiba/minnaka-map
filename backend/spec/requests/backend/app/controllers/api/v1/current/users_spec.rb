a2c5209aa9790dbaabe17efa94e0cc5d485b49cf
Teri <erk.nishimura@gmail.com>
2024-11-14 15:14:19 +0900

    delete

--- backend/spec/requests/backend/app/controllers/api/v1/current/users_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Backend::App::Controllers::Api::V1::Current::Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
