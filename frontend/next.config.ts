bd5f9dc15baa0e326f4e8914b45ba26c6f5bf799
Teri <erk.nishimura@gmail.com>
2024-11-18 16:50:31 +0900

    fix: js -> ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
