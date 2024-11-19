aeb345e43f2a8d19d3e83e4fd330f260559ca582
Teri <erk.nishimura@gmail.com>
2024-11-19 16:54:04 +0900

    add: eslint rules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,5 +1,16 @@
 import antfu from '@antfu/eslint-config'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 
-export default antfu({
-
-})
+export default antfu(
+  {
+    react: true,
+    ignores: [],
+  },
+  {
+    files: ['src/app/components/ui/forms/**/*.tsx'],
+    rules: {
+      'node/prefer-global/process': 'off',
+    },
+  },
+  ...tailwindcss.configs['flat/recommended'],
+)
