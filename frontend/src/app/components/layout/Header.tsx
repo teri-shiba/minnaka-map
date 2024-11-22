46ee7a2263acafddc7740241398ae149f831affd
Teri <erk.nishimura@gmail.com>
2024-11-22 15:28:19 +0900

    fix: change logo image

--- frontend/src/app/components/layout/Header.tsx
+++ frontend/src/app/components/layout/Header.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
 import Link from 'next/link'
+import Logo from '~/public/logo.svg'
 import { Auth } from '../ui/Auth'
 
 export default function Header() {
@@ -7,7 +7,11 @@ export default function Header() {
     <header className="supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full backdrop-blur">
       <div className="mx-auto flex h-16 items-center justify-between gap-4 px-5 xl:container md:px-6">
         <Link href="/" className="flex items-center space-x-2">
-          <Image src="/logo.svg" alt="minnaka map" width={225} height={25} />
+          <Logo
+            width={220}
+            height={35}
+            style={{ display: 'block' }}
+          />
         </Link>
         <Auth />
       </div>
