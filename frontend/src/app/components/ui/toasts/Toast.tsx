e1c6c28bb337a1f8fd20476a1b3ec52fe70d1f3a
Teri <erk.nishimura@gmail.com>
2024-12-09 16:18:40 +0900

    refactor: テーマを一旦削除

--- frontend/src/app/components/ui/toasts/Toast.tsx
+++ frontend/src/app/components/ui/toasts/Toast.tsx
@@ -1,16 +1,11 @@
 'use client'
-
-import { useTheme } from 'next-themes'
 import { Toaster as Sonner } from 'sonner'
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
 
 function Toaster({ ...props }: ToasterProps) {
-  const { theme = 'system' } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps['theme']}
       className="group"
       toastOptions={{
         classNames: {
