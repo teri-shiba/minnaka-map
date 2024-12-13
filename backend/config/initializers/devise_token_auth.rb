e956f00dad64871efb89e78ae7525a343f38876f
Teri <erk.nishimura@gmail.com>
2024-12-13 14:52:50 +0900

    add: Cookiesへの保存を実装

--- backend/config/initializers/devise_token_auth.rb
+++ backend/config/initializers/devise_token_auth.rb
@@ -63,4 +63,12 @@ DeviseTokenAuth.setup do |config|
   # devise confirmable module. If you want to use devise confirmable module and
   # send email, set it to true. (This is a setting for compatibility)
   # config.send_confirmation_email = true
+
+  # setting cookie
+  config.cookie_enabled=true
+  config.cookie_attributes={
+    httponly: true,
+    secure: true,
+  }
+
 end
