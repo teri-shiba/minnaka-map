ac0e26c8561cd2cfdda130e8d366cdccb5fbee7d
Teri <erk.nishimura@gmail.com>
2024-12-20 14:56:20 +0900

    add: ユーザーのログイン状況をフェッチ

--- frontend/src/app/hooks/useUser.ts
+++ frontend/src/app/hooks/useUser.ts
@@ -1,8 +1,12 @@
 import type { AxiosResponse } from 'axios'
 import axios, { isAxiosError } from 'axios'
+import { useAtom } from 'jotai'
 import useSWR from 'swr'
+import { userStateAtom } from '../lib/state/userStateAtom'
 
 export default function useUser() {
+  const [, setUser] = useAtom(userStateAtom)
+
   const fetcher = async (url: string) => {
     try {
       const res: AxiosResponse = await axios.get(url, {
@@ -10,6 +14,20 @@ export default function useUser() {
         withCredentials: true,
       })
 
+      if (res.data.login !== false) {
+        setUser({
+          ...res.data,
+          isSignedIn: true,
+          isLoading: false,
+        })
+      }
+      else {
+        setUser({
+          ...res.data,
+          isSignedIn: false,
+          isLoading: false,
+        })
+      }
       return res.data
     }
     catch (e) {
@@ -21,7 +39,7 @@ export default function useUser() {
   }
 
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL
-  const { data, error, isLoading } = useSWR(`${baseURL}/current/user/show_status`, fetcher)
+  const { data, error, isLoading } = useSWR(`${baseURL}/current/user/show_status`, fetcher, { revalidateOnFocus: false })
 
   return {
     isLogin: data?.login !== false,
