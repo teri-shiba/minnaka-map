d884a30dc4fa5f571a12e7726b3c954af2da2770
Teri <erk.nishimura@gmail.com>
2024-12-20 14:53:52 +0900

    delete

--- backend/config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.config.session_store :cookie_store,
-  key: '_session_id',
-  httponly: true,
-  secure: Rails.env.production?, # 本番環境でのみSecure属性を有効化
-  same_site: :lax,
-  expire_after: 30.minutes
