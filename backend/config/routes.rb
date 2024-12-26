1b5ec5526934d66b882e7c869e151d6fe2851b42
Teri <erk.nishimura@gmail.com>
2024-12-26 15:20:31 +0900

    add: 新規登録に伴うメール認証機能を実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "UserAuth", at: "auth", controllers: {
         sessions: "api/v1/auth/sessions"
       }
-
+      namespace :user do
+        resource :confirmations, only: [:update]
+      end
       namespace :current do
         resource :user, only: [:show] do
           get :show_status, on: :member
