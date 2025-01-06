fd72c2a3a5defac6b8ea59ba1e83874e9e4860c6
Teri <erk.nishimura@gmail.com>
2025-01-06 15:48:25 +0900

    #29 fix: Userの作成に伴い、UserAuthにUserの外部キーを設定

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_13_064518) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_27_062703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,9 +33,19 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_13_064518) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["confirmation_token"], name: "index_user_auths_on_confirmation_token", unique: true
     t.index ["email"], name: "index_user_auths_on_email", unique: true
     t.index ["reset_password_token"], name: "index_user_auths_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_user_auths_on_uid_and_provider", unique: true
+    t.index ["user_id"], name: "index_user_auths_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "user_auths", "users"
 end
