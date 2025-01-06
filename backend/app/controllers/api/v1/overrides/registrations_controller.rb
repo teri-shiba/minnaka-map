99e3efa59004ec088826adde41d369dff74e5f83
Teri <erk.nishimura@gmail.com>
2025-01-06 17:21:21 +0900

    #29 fix: UserAuthからnameを削除し、Userのみにnameを保存

--- backend/app/controllers/api/v1/overrides/registrations_controller.rb
+++ backend/app/controllers/api/v1/overrides/registrations_controller.rb
@@ -1,17 +1,23 @@
 class Api::V1::Overrides::RegistrationsController < DeviseTokenAuth::RegistrationsController
   def build_resource
-    super
+    user_auth_params = sign_up_params.except(:name)
+
+    @resource = resource_class.new(user_auth_params)
+    @resource.provider = provider
+
+    @resource.email = if resource_class.case_insensitive_keys.include?(:email)
+                        user_auth_params[:email]&.downcase
+                      else
+                        user_auth_params[:email]
+                      end
+
     user = User.create!(name: sign_up_params[:name])
     @resource.user_id = user.id
   end
 
-  def create
-    super
-  end
-
   private
 
-  def sign_up_params
-    params.permit(:email, :password, :password_confirmation, :user_id)
-  end
+    def sign_up_params
+      params.permit(:email, :password, :password_confirmation, :name, :user_id)
+    end
 end
