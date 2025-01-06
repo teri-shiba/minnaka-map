c64ed791f302bbf50a22e8f54f9b968a474dc0bd
Teri <erk.nishimura@gmail.com>
2025-01-06 15:48:25 +0900

    #29 fix: Userの作成に伴い、UserAuthにUserの外部キーを設定

---
+++ backend/app/controllers/api/v1/overrides/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::Overrides::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  def build_resource
+    super
+    user = User.create!(name: sign_up_params[:name])
+    @resource.user_id = user.id
+  end
+
+  def create
+    super
+  end
+
+  private
+
+  def sign_up_params
+    params.permit(:email, :password, :password_confirmation, :name, :user_id)
+  end
+end
