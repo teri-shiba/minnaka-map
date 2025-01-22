d92a1edee8d633d5c84cebf04e4c89188a5d3ee6
Teri <erk.nishimura@gmail.com>
2025-01-22 15:02:43 +0900

    #31 add: omniauthでSNS認証を実装

---
+++ backend/config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.logger = Rails.logger
+  provider :google_oauth2,
+           Rails.application.credentials.google[:client_id],
+           Rails.application.credentials.google[:client_secret]
+  OmniAuth.config.allowed_request_methods = %i[get]
+  OmniAuth.config.silence_get_warning = true
+end
