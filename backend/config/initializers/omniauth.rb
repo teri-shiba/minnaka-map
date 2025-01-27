30af2397f6317cff8e233b56f36315d62f4934dd
Teri <erk.nishimura@gmail.com>
2025-01-27 17:00:06 +0900

    #31 add: LINE認証を追加

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -5,6 +5,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            Rails.application.credentials.google[:client_id],
            Rails.application.credentials.google[:client_secret]
+  provider :line,
+            Rails.application.credentials.line[:channel_id],
+            Rails.application.credentials.line[:channel_secret]
   OmniAuth.config.allowed_request_methods = %i[get]
   OmniAuth.config.silence_get_warning = true
 end
