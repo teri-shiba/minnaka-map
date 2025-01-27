57296d3c5dae43c8240469a287f76452816acaf5
Teri <erk.nishimura@gmail.com>
2025-01-27 13:42:25 +0900

    #31 fix: 条件分岐による機能を明確化した

--- frontend/src/app/hooks/useOAuthCallback.ts
+++ frontend/src/app/hooks/useOAuthCallback.ts
@@ -1,11 +1,13 @@
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { toast } from 'sonner'
+import { useAuth } from './useAuth'
 
 export default function useOAuthCallback() {
   const router = useRouter()
   const params = useSearchParams()
   const [status, setStatus] = useState<string | null>(null)
+  const { fetchUser, logout } = useAuth()
 
   useEffect(() => {
     const currentStatus = params.get('status')
@@ -16,16 +18,24 @@ export default function useOAuthCallback() {
     if (!status)
       return
 
-    if (status === 'success') {
-      toast.success('ログインに成功しました')
-    }
-    else if (status === 'error') {
-      toast.error('ログインに失敗しました')
-    }
-    else {
-      toast.error('不正なアクセスです')
+    const fetchData = async () => {
+      if (status === 'success') {
+        toast.success('ログインに成功しました')
+        await fetchUser()
+      }
+      else if (status === 'error') {
+        toast.error('ログインに失敗しました')
+        await logout()
+      }
+      else {
+        toast.error('不正なアクセスです')
+        await logout()
+      }
+
+      router.push('/')
     }
 
-    router.push('/')
-  }, [status, router])
+    fetchData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [status])
 }
