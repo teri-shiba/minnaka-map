7a73aaf4a664f02b697fbb17dfa91fd1e9034190
Teri <erk.nishimura@gmail.com>
2025-01-28 17:15:31 +0900

    refactor: rubocop

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -6,8 +6,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
            Rails.application.credentials.google[:client_id],
            Rails.application.credentials.google[:client_secret]
   provider :line,
-            Rails.application.credentials.line[:channel_id],
-            Rails.application.credentials.line[:channel_secret]
+           Rails.application.credentials.line[:channel_id],
+           Rails.application.credentials.line[:channel_secret]
   OmniAuth.config.allowed_request_methods = %i[get]
   OmniAuth.config.silence_get_warning = true
 end
