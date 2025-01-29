03b7a7e3c2cfcbb052e21231d9fd76b74158376a
Teri <erk.nishimura@gmail.com>
2025-01-29 17:40:49 +0900

    refactor: png -> webp

--- frontend/src/app/components/layout/Header.tsx
+++ frontend/src/app/components/layout/Header.tsx
@@ -1,8 +1,10 @@
 'use client'
 import { useAtom } from 'jotai'
+import Image from 'next/image'
 import Link from 'next/link'
 import { userStateAtom } from '~/app/lib/state/userStateAtom'
-import Logo from '~/public/logo.svg'
+import logo from '~/public/logo.webp'
+import logoMark from '~/public/logo_mark.webp'
 import { Auth } from '../ui/Auth'
 import UserMenu from '../ui/dropdownmenu/UserMenu'
 import { Skeleton } from '../ui/skeleton/Skeleton'
@@ -11,12 +13,21 @@ export default function Header() {
   const [user] = useAtom(userStateAtom)
   return (
     <header className="supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full backdrop-blur">
-      <div className="mx-auto flex h-16 items-center justify-between gap-4 px-5 xl:container md:px-6">
+      <div className="mx-auto flex h-16 items-center justify-between gap-4 xl:container">
         <Link href="/" className="flex items-center space-x-2">
-          <Logo
-            width={220}
-            height={35}
-            style={{ display: 'block' }}
+          <Image
+            alt="minnaka map"
+            src={logo}
+            width={224}
+            height={28}
+            className="hidden sm:block"
+          />
+          <Image
+            alt="minnaka map"
+            src={logoMark}
+            width={28}
+            height={28}
+            className="block sm:hidden"
           />
         </Link>
         { user.isLoading
