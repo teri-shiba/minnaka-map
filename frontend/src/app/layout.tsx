51fc9c274e8daa52c88b23be110aa9e958ef396a
Teri <erk.nishimura@gmail.com>
2025-01-31 14:15:21 +0900

    refactor: components へのパスを追加し、修正

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import ClientAuthProvider from './components/layout/ClientAuthProvider'
+import ClientAuthProvider from '~/components/layout/ClientAuthProvider'
 import './styles/globals.css'
 
 export const metadata: Metadata = {
