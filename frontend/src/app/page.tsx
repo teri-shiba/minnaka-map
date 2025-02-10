288eb504ca74e1de9957d30f5267920047916418
Teri <erk.nishimura@gmail.com>
2025-02-10 18:15:37 +0900

    refactor: レイアウトの調整

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,16 +2,20 @@
 import Image from 'next/image'
 import { LuPlus } from 'react-icons/lu'
 import { Button } from '~/components/ui/buttons/Button'
+import { DesktopGuideCarousel } from '~/components/ui/carousels/DesktopGuideCarousel'
+import { MobileGuideCarousel } from '~/components/ui/carousels/MobileGuideCarousel'
+
+// TODO：画像変数のインポートが多いので、まとめてインポートする構造に変更する
+import wavePC from '~/public/figure_wave_pc.webp'
+import waveSP from '~/public/figure_wave_sp.webp'
 import featureFriends from '~/public/image_feature_01.webp'
 import featureDate from '~/public/image_feature_02.webp'
-
 import featureBusiness from '~/public/image_feature_03.webp'
 import logo from '~/public/logo.webp'
 import mvPC from '~/public/mv_01_pc.webp'
 import mvSP from '~/public/mv_01_sp.webp'
 
-import { DesktopGuideCarousel } from './components/ui/carousels/DesktopGuideCarousel'
-import { MobileGuideCarousel } from './components/ui/carousels/MobileGuideCarousel'
+import { Section } from './components/layout/Section'
 import useConfirmEmail from './hooks/useConfirmEmail'
 import useOAuthCallback from './hooks/useOAuthCallback'
 
@@ -21,48 +25,68 @@ export default function Home() {
 
   return (
     <>
-      <section className="flex flex-col items-center justify-center bg-secondary py-12 md:flex-row md:justify-between md:py-4">
-        <div className="">
-          <h2 className="text-center md:text-left">
-            <span className="text-lg text-secondary-foreground sm:text-xl">みんなのまんなか</span>
+      <Section className="bg-secondary py-12 md:py-4">
+        <div className="flex flex-col items-center justify-center md:flex-row md:justify-between">
+          <div>
+            <h2 className="text-center md:text-left">
+              <span className="text-lg text-secondary-foreground sm:text-xl">みんなのまんなか</span>
+              <Image
+                alt="minnaka map"
+                src={logo}
+                width={224}
+                height={28}
+                className=""
+              />
+            </h2>
+
+            {/* フォームはあとで実装 */}
+
+            <Button variant="link" className="text-secondary-foreground">
+              <LuPlus className="stroke-[3]" />
+              3人目を追加する
+            </Button>
+
+            <Button>検索する</Button>
+          </div>
+
+          <div>
             <Image
-              alt="minnaka map"
-              src={logo}
-              width={224}
-              height={28}
-              className=""
+              alt="mv-pc"
+              src={mvPC}
+              width={424}
+              height={536}
+              className="hidden md:block"
             />
-          </h2>
 
-          {/* フォームはあとで実装 */}
-
-          <Button variant="link" className="text-secondary-foreground">
-            <LuPlus className="stroke-[3]" />
-            3人目を追加する
-          </Button>
-
-          <Button>検索する</Button>
+            <Image
+              alt="mv-sp"
+              src={mvSP}
+              width={264}
+              height={184}
+              className="block md:hidden"
+            />
+          </div>
         </div>
+      </Section>
 
-        <div>
-          <Image
-            alt="mv-pc"
-            src={mvPC}
-            width={424}
-            height={536}
-            className="hidden md:block"
-          />
+      <div>
+        <Image
+          alt=""
+          src={wavePC}
+          width={1280}
+          height={96}
+          className="hidden h-24 w-full md:block"
+        />
+        <Image
+          alt=""
+          src={waveSP}
+          width={393}
+          height={48}
+          className="block h-12 w-full md:hidden"
+        />
+      </div>
 
-          <Image
-            alt="mv-sp"
-            src={mvSP}
-            width={264}
-            height={184}
-            className="block md:hidden"
-          />
-        </div>
-      </section>
-      <section className="py-14 md:py-24">
+      <Section className="py-14 md:py-24">
         <h2 className="mb-7 text-center text-lg text-secondary-foreground sm:text-2xl md:mb-12">
           <span className="inline-block align-text-bottom">
             <Image
@@ -129,7 +153,7 @@ export default function Home() {
             </p>
           </div>
         </div>
-      </section>
+      </Section>
 
       <MobileGuideCarousel />
       <DesktopGuideCarousel />
