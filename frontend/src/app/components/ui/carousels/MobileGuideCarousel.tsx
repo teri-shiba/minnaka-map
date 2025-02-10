7107f5ec5081e6cad66b3df7deab7173c0114c0b
Teri <erk.nishimura@gmail.com>
2025-02-10 16:12:44 +0900

    refactor: 微修正

--- frontend/src/app/components/ui/carousels/MobileGuideCarousel.tsx
+++ frontend/src/app/components/ui/carousels/MobileGuideCarousel.tsx
@@ -25,15 +25,17 @@ const carouselPlugins = [
 export function MobileGuideCarousel() {
   return (
     <section className="relative block py-8 before:absolute before:left-0 before:top-0 before:z-[-1] before:h-2/3 before:w-full before:rounded-2xl before:bg-secondary md:hidden">
-      <h2 className="mb-7 flex items-center justify-center text-center text-lg text-secondary-foreground sm:text-2xl md:mb-12">
-        <Image
-          alt="minnaka map"
-          src={logo}
-          width={170}
-          height={30}
-          className="w-44 sm:w-60"
-        />
-        <span className="pl-1">の使い方</span>
+      <h2 className="mb-7 text-center text-lg text-secondary-foreground sm:text-2xl md:mb-12">
+        <span className="inline-block align-text-bottom">
+          <Image
+            alt="minnaka map"
+            src={logo}
+            width={170}
+            height={30}
+            className="w-44 sm:w-60"
+          />
+        </span>
+        <span className="inline-block pl-0.5">の使い方</span>
       </h2>
       <Carousel opts={carouselOptions} plugins={carouselPlugins}>
         <CarouselContent>
@@ -41,13 +43,15 @@ export function MobileGuideCarousel() {
             <CarouselItem key={data.id}>
               <Card className="bg-transparent">
                 <CardContent className="mx-5 flex flex-col items-center justify-center">
-                  <Image
-                    alt={data.title}
-                    src={data.imageUrl}
-                    width={280}
-                    height={380}
-                    className="mb-5 drop-shadow-lg"
-                  />
+                  <div className="mb-5 h-96 w-72 overflow-hidden drop-shadow-lg">
+                    <Image
+                      alt={data.title}
+                      src={data.imageUrl}
+                      width={280}
+                      height={380}
+                      className=""
+                    />
+                  </div>
                   <h3 className="mb-2 flex gap-2 text-base text-secondary-foreground">
                     <span className="flex size-6 items-center justify-center rounded-sm bg-primary text-sm text-white">
                       {data.id}
