02b4174b3d4401e6af7eceebc7544e1f4ae4e957
Teri <erk.nishimura@gmail.com>
2025-02-12 09:31:42 +0900

    refactor: 画像をモジュールで管理

---
+++ frontend/src/app/lib/image/images.ts
@@ -0,0 +1,19 @@
+import wavePC from '~/public/figure_wave_pc.webp'
+import waveSP from '~/public/figure_wave_sp.webp'
+import featureFriends from '~/public/image_feature_01.webp'
+import featureDate from '~/public/image_feature_02.webp'
+import featureBusiness from '~/public/image_feature_03.webp'
+import logo from '~/public/logo.webp'
+import mvPC from '~/public/mv_01_pc.webp'
+import mvSP from '~/public/mv_01_sp.webp'
+
+export const images = {
+  wavePC,
+  waveSP,
+  featureFriends,
+  featureDate,
+  featureBusiness,
+  logo,
+  mvPC,
+  mvSP,
+}
