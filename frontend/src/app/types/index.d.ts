06235d99cbd984b16f676a44fcc9c2ab8aed6e6a
Teri <erk.nishimura@gmail.com>
2025-02-13 16:24:38 +0900

    #36 add: エリア検索フォームを追加

--- frontend/src/app/types/index.d.ts
+++ frontend/src/app/types/index.d.ts
@@ -1,4 +1,9 @@
+import type { z } from 'zod'
+import type { areaFormSchema } from '../lib/shemas/areaSearchSchema'
+
 declare module '*.svg' {
   const content: React.FC<React.SVGProps<SVGElement>>
   export default content
 }
+
+export type AreaFormValues = z.infer<typeof areaFormSchema>
