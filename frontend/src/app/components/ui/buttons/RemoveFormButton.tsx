f8c4b298e7710dc5d40b1320f43e798da96349bf
Teri <erk.nishimura@gmail.com>
2025-02-13 16:24:38 +0900

    #36 add: エリア検索フォームを追加

---
+++ frontend/src/app/components/ui/buttons/RemoveFormButton.tsx
@@ -0,0 +1,20 @@
+import { LuMinus } from 'react-icons/lu'
+import { Button } from './Button'
+
+interface FormRemoveButtonProps {
+  index: number
+  remove: (index?: number | number[]) => void
+}
+
+export function RemoveFormButton({ index, remove }: FormRemoveButtonProps) {
+  return (
+    <Button
+      type="button"
+      size="icon"
+      onClick={() => { remove(index) }}
+      className="absolute right-2 top-1/2 !mt-0 size-5 -translate-y-1/2 rounded-full bg-gray-300 font-bold hover:bg-gray-500"
+    >
+      <LuMinus className="!size-3 stroke-[3]" />
+    </Button>
+  )
+}
