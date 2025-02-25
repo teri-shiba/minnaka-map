6a011340a648759564730bad5c39aee2d915d5dc
Teri <erk.nishimura@gmail.com>
2025-02-25 14:27:55 +0900

    fix: SearchHistoryCenterLocation モデルと関連付け

--- backend/app/models/search_history.rb
+++ backend/app/models/search_history.rb
@@ -1,4 +1,5 @@
 class SearchHistory < ApplicationRecord
   belongs_to :user
   has_many :search_history_start_locations, dependent: :destroy
+  has_one :search_history_center_location, dependent: :destroy
 end
