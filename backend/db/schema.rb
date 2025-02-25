c56cbaaf5a06b6bf3305eeda4538c8a6d41b9aa3
Teri <erk.nishimura@gmail.com>
2025-02-25 15:28:27 +0900

    fix: Prefecture テーブルと Station テーブルに NULL 制約を追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_25_055508) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_25_060136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_25_055508) do
   end
 
   create_table "prefectures", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_25_055508) do
   end
 
   create_table "stations", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "location_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
