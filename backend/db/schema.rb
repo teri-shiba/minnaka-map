c6159dfe8e9959d14a32c92d35561ec77c049483
Teri <erk.nishimura@gmail.com>
2025-02-25 10:23:47 +0900

    add: Prefecture モデルを作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_06_074526) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_25_005152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_auths", force: :cascade do |t|
     t.string "provider", default: "email", null: false
