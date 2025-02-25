78e2d74a581ad6cca599f52feec4884d2f35a830
Teri <erk.nishimura@gmail.com>
2025-02-25 14:54:39 +0900

    fix: Location テーブルの place_type にデフォルト値 `address` を設定

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_02_25_040910) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_25_055123) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_02_25_040910) do
     t.decimal "longitude", precision: 10, scale: 5
     t.string "locality"
     t.string "sublocality"
-    t.string "place_type"
+    t.string "place_type", default: "address"
     t.bigint "prefecture_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
