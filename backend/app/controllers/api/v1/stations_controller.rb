2e0ab61bc97ea46e88e69539c55486d50ac5fc09
Teri <erk.nishimura@gmail.com>
2025-03-26 14:18:26 +0900

    fix: station -> stations （フロントと一致させる）

--- backend/app/controllers/api/v1/stations_controller.rb
+++ backend/app/controllers/api/v1/stations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::StationsController < ApplicationController
   def index
     query = params[:q]&.strip
-    return render json: { station: [] } if query.blank?
+    return render json: { stations: [] } if query.blank?
 
     stations = Station.where("name ILIKE ?", "%#{query}").limit(5)
     render json: { stations: stations.map {|s| { id: s.id, name: s.name, latitude: s.latitude, longitude: s.longitude } } }
