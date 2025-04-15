d759faefbfe68e98947f11ae8f10d9b2c77065a8
Teri <erk.nishimura@gmail.com>
2025-04-15 16:59:20 +0900

    fix: SNS認証の各情報を分けて管理

--- frontend/src/app/components/ui/drawers/AuthDrawer.tsx
+++ frontend/src/app/components/ui/drawers/AuthDrawer.tsx
@@ -1,9 +1,9 @@
 'use client'
 
+import Image from 'next/image'
 import { useState } from 'react'
 import * as React from 'react'
-import Google from '~/public/google.svg'
-import Line from '~/public/line.svg'
+import { authProviders } from '~/app/lib/authConstants'
 import Mark from '~/public/mark.svg'
 import { Button } from '../buttons/Button'
 import LoginForm from '../forms/LoginForm'
@@ -19,6 +19,7 @@ import {
 export function AuthDrawer() {
   const [open, setOpen] = useState(false)
   const [isLogin, setIsLogin] = useState(true)
+  const baseApiURL = process.env.NEXT_PUBLIC_API_BASE_URL
 
   const handleClick = () => {
     setIsLogin(!isLogin)
@@ -50,23 +51,22 @@ export function AuthDrawer() {
           <p className="relative mx-5 text-center text-sm text-gray-500 before:absolute before:left-0 before:top-1/2 before:-z-10 before:h-px before:w-full before:bg-gray-300">
             <span className="inline-block bg-white px-4">OR</span>
           </p>
-          <div className="grid grid-cols-2 gap-3">
-            <Button variant="outline" className="h-auto py-3 [&_svg]:size-6" aria-label="Google">
-              <Google
-                width={24}
-                height={24}
-                fill="none"
-                style={{ display: 'block' }}
-              />
-            </Button>
-            <Button variant="outline" className="h-auto py-3 [&_svg]:size-6" aria-label="LINE">
-              <Line
-                width={24}
-                height={24}
-                fill="none"
-                style={{ display: 'block' }}
-              />
-            </Button>
+          <div className="flex gap-3 max-[383px]:block">
+            {authProviders.map(provider => (
+              <a
+                key={provider.name}
+                href={`${baseApiURL}/provider/${provider.authUrl}`}
+                className="flex h-auto w-full items-center justify-center gap-2 rounded-md border border-input py-3 text-sm font-bold transition-colors hover:bg-accent max-[383px]:[&:not(:last-child)]:mb-3"
+              >
+                <Image
+                  src={provider.iconImg}
+                  width={18}
+                  height={18}
+                  alt=""
+                />
+                {isLogin ? `${provider.name}でログイン` : `${provider.name}で登録`}
+              </a>
+            ))}
           </div>
           <Button onClick={handleClick} className="mx-auto my-4 inline-block h-auto !bg-transparent py-0 text-sm font-normal text-foreground hover:text-blue-500 hover:underline">
             {isLogin ? '新規会員登録はこちら' : 'ログインはこちら'}
