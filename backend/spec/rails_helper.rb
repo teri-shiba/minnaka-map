e672e9f33b8bbd1f15766a37032b684abd31b8a0
Teri <erk.nishimura@gmail.com>
2025-04-15 09:43:41 +0900

    refactor: typo should -> shoulda / congifure -> configure

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -9,7 +9,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'should/matchers'
+require 'shoulda/matchers'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -68,7 +68,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
-Shoulda::Matchers.congifure do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
