cf1f84d992c18f04327880b3e531f556e16b1940
Teri <erk.nishimura@gmail.com>
2025-04-23 14:44:32 +0900

    fix: テスト環境では認証プロバイダをスキップ

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,16 +1,16 @@
-# return if Rails.env.test?
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.logger = Rails.logger
-  provider :google_oauth2,
-           Rails.application.credentials.google[:client_id],
-           Rails.application.credentials.google[:client_secret]
+  unless Rails.env.test?
+    provider :google_oauth2,
+             Rails.application.credentials.google[:client_id],
+             Rails.application.credentials.google[:client_secret]
 
-  require "omniauth/strategies/line"
-  provider :line,
-           Rails.application.credentials.line[:channel_id],
-           Rails.application.credentials.line[:channel_secret],
-           scope: "profile openid email"
+    require "omniauth/strategies/line"
+    provider :line,
+             Rails.application.credentials.line[:channel_id],
+             Rails.application.credentials.line[:channel_secret],
+             scope: "profile openid email"
+  end
 
   OmniAuth.config.allowed_request_methods = %i[get]
   OmniAuth.config.silence_get_warning = true
