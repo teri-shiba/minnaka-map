ed89549c8d57f3afcf3cdac2b7d1e8b5b8dc5a5e
Teri <erk.nishimura@gmail.com>
2025-04-23 14:39:17 +0900

    fix: 初期化を実行

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,10 +1,11 @@
-return if Rails.env.test?
+# return if Rails.env.test?
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.logger = Rails.logger
   provider :google_oauth2,
            Rails.application.credentials.google[:client_id],
            Rails.application.credentials.google[:client_secret]
+
   require "omniauth/strategies/line"
   provider :line,
            Rails.application.credentials.line[:channel_id],
