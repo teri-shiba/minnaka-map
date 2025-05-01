b3f78a7a342bfdd7c58d123345d8d7aebfd45680
Teri <erk.nishimura@gmail.com>
2025-05-01 16:38:54 +0900

    fix: モデル名を変更 `_location` -> `_station`

--- backend/spec/models/search_history_center_station_spec.rb
+++ backend/spec/models/search_history_center_station_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe SearchHistoryCenterLocation, type: :model do
+RSpec.describe SearchHistoryCenterStation, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
