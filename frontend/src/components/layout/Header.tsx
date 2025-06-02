972d7cd2eb572142665cccd2b4ad33d932fe4ffb
Teri <erk.nishimura@gmail.com>
2025-06-02 18:35:30 +0900

    fix: `/app` -> `/src` に移動

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,58 +1,54 @@
 'use client'
-import { useAtom } from 'jotai'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { userStateAtom } from '~/app/lib/state/userStateAtom'
 import logo from '~/public/logo.webp'
 import logoMark from '~/public/logo_mark.webp'
-import { Auth } from '../ui/Auth'
-import UserMenu from '../ui/dropdownmenu/UserMenu'
+import { useFetchUser } from '../../hooks/useFetchUser'
+import { AuthDialog } from '../ui/dialogs/AuthDialog'
 import { Skeleton } from '../ui/skeleton/Skeleton'
 
 export default function Header() {
-  const [user] = useAtom(userStateAtom)
-  const currentPath = usePathname()
-  const isHomePage = currentPath === '/'
+  const pathName = usePathname()
+  const isHomePage = pathName === '/'
   const headerBgClass = isHomePage ? 'bg-secondary' : ''
-
-  let userStatusUI = null
-  if (user.isLoading) {
-    userStatusUI = (
-      <Skeleton className="h-[40px] w-[87px] rounded-full" />
-    )
-  }
-  else if (!user.isSignedIn) {
-    userStatusUI = <Auth />
-  }
-  else {
-    userStatusUI = <UserMenu />
-  }
+  const { user, isLoading } = useFetchUser()
 
   return (
     <header className={`w-full ${headerBgClass}`}>
       <div className="mx-auto flex h-16 max-w-screen-lg items-center justify-between gap-4 px-5">
         <Link href="/" className="flex items-center">
+          {/* TODO: Image タグをひとつにして、切り替える */}
           <Image
-            aria-label="Go to Home"
-            alt="logo"
             src={logo}
+            alt="logo"
             width={224}
             height={29}
             priority
+            aria-label="Go to Home"
             className="hidden h-auto md:block"
           />
           <Image
-            aria-label="Go to Home"
-            alt="logo"
             src={logoMark}
+            alt="logo"
             width={28}
             height={28}
             priority
+            aria-label="Go to Home"
             className="block h-auto md:hidden"
           />
         </Link>
-        {userStatusUI}
+        {isLoading
+          ? <Skeleton className="h-[40px] w-[87px] rounded-full" />
+          : user
+            ? <p>ログイン済</p>
+            : <AuthDialog />}
+        {/* {isLoading
+          ? <Skeleton className="h-[40px] w-[87px] rounded-full" />
+          : user
+            ? <UserMenu />
+            : <Auth />
+        } */}
       </div>
     </header>
   )
--- frontend/src/components/ui/buttons/ResetFormButton.tsx
+++ frontend/src/components/ui/buttons/ResetFormButton.tsx
@@ -1,5 +1,5 @@
 import type { UseFormReturn } from 'react-hook-form'
-import type { AreaFormValues } from '~/app/lib/schemas/areaSearchSchema'
+import type { AreaFormValues } from '~/lib/schemas/areaSearchSchema'
 import { LuX } from 'react-icons/lu'
 import { Button } from './Button'
 
