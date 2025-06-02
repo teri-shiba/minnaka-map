7fd7367801454503a7c32c8a6c48e7d2a82f61bc
Teri <erk.nishimura@gmail.com>
2025-06-02 18:14:53 +0900

    fix: ディレクトリ階層に誤りがあったため、(pages) から外に出す

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 import Image from 'next/image'
 import Section from '~/components/layout/Section'
-import { DesktopGuideCarousel } from '~/components/ui/carousels/DesktopGuideCarousel'
-import { MobileGuideCarousel } from '~/components/ui/carousels/MobileGuideCarousel'
 import StetionSearchForm from '~/components/ui/forms/StationSearchForm'
 import useConfirmEmail from '~/hooks/useConfirmEmail'
+import useOAuthCallback from '~/hooks/useOAuthCallback'
 import { images } from '~/lib/image/images'
-import useOAuthCallback from '../hooks/useOAuthCallback'
 
 export default function Home() {
   useOAuthCallback()
@@ -143,8 +141,8 @@ export default function Home() {
         </div>
       </Section>
 
-      <MobileGuideCarousel />
-      <DesktopGuideCarousel />
+      {/* <MobileGuideCarousel />
+      <DesktopGuideCarousel /> */}
     </>
   )
 }
