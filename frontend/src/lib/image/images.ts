f142238006c845c33b5449b7f3e5bbf22ee6e393
Teri <erk.nishimura@gmail.com>
2025-06-04 16:34:30 +0900

    delete: wave を削除

--- frontend/src/lib/image/images.ts
+++ frontend/src/lib/image/images.ts
@@ -1,5 +1,3 @@
-import wavePC from '~/public/figure_wave_pc.webp'
-import waveSP from '~/public/figure_wave_sp.webp'
 import featureFriends from '~/public/image_feature_01.webp'
 import featureDate from '~/public/image_feature_02.webp'
 import featureBusiness from '~/public/image_feature_03.webp'
@@ -8,8 +6,6 @@ import mvPC from '~/public/mv_01_pc.webp'
 import mvSP from '~/public/mv_01_sp.webp'
 
 export const images = {
-  wavePC,
-  waveSP,
   featureFriends,
   featureDate,
   featureBusiness,
