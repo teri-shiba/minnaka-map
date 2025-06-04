311d082729a54087665c975d4d0b459ff8544c67
Teri <erk.nishimura@gmail.com>
2025-06-04 18:11:01 +0900

    fix: ファイル名変更 && axios インスタンスを作成しリファクタリング

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
   withCredentials: true,
   timeout: 5000,
 })
