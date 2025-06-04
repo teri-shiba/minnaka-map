d797aad9d5fde6efdb2b3452aafcf410729ea29d
Teri <erk.nishimura@gmail.com>
2025-06-04 18:24:08 +0900

    fix: ロゴ画像を変数で管理

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,40 +1,64 @@
+'use client'
+import type { StaticImageData } from 'next/image'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useFetchUser } from '~/hooks/useFetchUser'
 import logo from '~/public/logo.webp'
 import logoMark from '~/public/logo_mark.webp'
+import { Auth } from '../ui/Auth'
+import UserMenu from '../ui/dropdownmenu/UserMenu'
+import { Skeleton } from '../ui/skeleton/Skeleton'
 
-export default async function Header() {
-  // const [user] = useAtom(userStateAtom)
+interface logoImages {
+  id: number
+  src: StaticImageData
+  width: number
+  height: number
+  customClassName: string
+}
+
+export default function Header() {
+  const { user, isLoading } = useFetchUser()
+
+  const logoImages: logoImages[] = [
+    {
+      id: 0,
+      src: logo,
+      width: 224,
+      height: 29,
+      customClassName: 'hidden md:block',
+    },
+    {
+      id: 1,
+      src: logoMark,
+      width: 28,
+      height: 28,
+      customClassName: 'block md:hidden',
+    },
+  ]
 
   return (
     <header className="w-full">
       <div className="mx-auto flex h-16 max-w-screen-lg items-center justify-between gap-4 px-5">
         <Link href="/" className="flex items-center">
-          {/* TODO: Image タグをひとつにして、切り替える */}
-          <Image
-            src={logo}
-            alt="logo"
-            width={224}
-            height={29}
-            priority
-            aria-label="Go to Home"
-            className="hidden h-auto md:block"
-          />
-          <Image
-            src={logoMark}
-            alt="logo"
-            width={28}
-            height={28}
-            priority
-            aria-label="Go to Home"
-            className="block h-auto md:hidden"
-          />
+          {logoImages.map(logo => (
+            <Image
+              key={logo.id}
+              src={logo.src}
+              alt="logo"
+              width={logo.width}
+              height={logo.height}
+              priority
+              aria-label="Go to Home"
+              className={logo.customClassName}
+            />
+          ))}
         </Link>
-        {/* {user.isSignedIn
-          // ? <Skeleton className="h-[40px] w-[87px] rounded-full" />
-          // : user
-          ? <UserMenu />
-          : <Auth />} */}
+        {isLoading
+          ? <Skeleton className="h-[40px] w-[87px] rounded-full" />
+          : user
+            ? <UserMenu />
+            : <Auth />}
       </div>
     </header>
   )
