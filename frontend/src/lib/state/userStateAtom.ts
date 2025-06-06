1517bf33ec6621af8988afa934be94070ef27c31
Teri <erk.nishimura@gmail.com>
2025-06-06 17:56:41 +0900

    fix: 初期値に戻すラッパー atomWithReset に変更し、isLoading は SWR を使用するため削除

--- frontend/src/lib/state/userStateAtom.ts
+++ frontend/src/lib/state/userStateAtom.ts
@@ -1,16 +1,11 @@
 import type { UserState } from '~/types/user'
-import { atom } from 'jotai'
+import { atomWithReset } from 'jotai/utils'
 
 export const initialUserState: UserState = {
   id: 0,
   name: '',
   email: '',
   isSignedIn: false,
-  isLoading: true,
 }
 
-export const userStateAtom = atom<UserState>(initialUserState)
-
-export const isSignedInAtom = atom(get => get(userStateAtom).isSignedIn)
-export const isLoadingAtom = atom(get => get(userStateAtom).isLoading)
-export const userNameAtom = atom(get => get(userStateAtom).name)
+export const userStateAtom = atomWithReset<UserState>(initialUserState)
