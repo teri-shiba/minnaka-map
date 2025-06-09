cca7ce3d0c25cc50ba7f11a040490fcf3098a121
Teri <erk.nishimura@gmail.com>
2025-06-09 14:06:06 +0900

    fix: axios インターセプターを使ってリファクタリング

--- frontend/src/hooks/useConfirmEmail.ts
+++ frontend/src/hooks/useConfirmEmail.ts
@@ -1,45 +1,38 @@
-import type { AxiosError } from 'axios'
-import axios from 'axios'
+'use client'
+
 import { useRouter, useSearchParams } from 'next/navigation'
-import { useEffect, useRef } from 'react'
+import { useEffect } from 'react'
 import { toast } from 'sonner'
+import api from '~/lib/api'
 
 export default function useConfirmEmail() {
   const router = useRouter()
-  const searchParams = useSearchParams()
-  const hasRun = useRef(false)
+  const params = useSearchParams()
 
   useEffect(() => {
-    if (hasRun.current)
+    const token = params.get('confirmation_token')
+    if (!token)
       return
-    hasRun.current = true
 
-    if (searchParams.has('confirmation_token')) {
-      const confirmationToken = searchParams.get('confirmation_token')
-      const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/user/confirmations`
-
-      axios.patch(url, { confirmation_token: confirmationToken })
-        .then(() => {
-          toast.success('メールアドレスの確認が完了しました')
-          router.replace('/')
-        })
-        .catch((e: AxiosError) => {
-          const status = e.response?.status
-          const message = '予期しないエラーが発生しました'
-
-          if (status === 404) {
-            toast.error('ユーザーが見つかりません')
-          }
-          else if (status === 400) {
-            toast.error('メールアドレスはすでに確認済みです')
-          }
-          else {
-            toast.error(message)
-          }
-        })
+    const handleConfirmEmail = async () => {
+      try {
+        await toast.promise(
+          api.patch('/user/confirmations', { confirmation_token: token }),
+          {
+            loading: '確認中…',
+            success: 'メールアドレスの確認が完了しました',
+            error: '確認に失敗しました',
+          },
+        )
+      }
+      catch (error) {
+        console.error('Confirm Email callback error:', error)
+      }
+      finally {
+        router.replace('/', { scroll: false })
+      }
     }
-    else {
-      toast.error('不正なアクセスです')
-    }
-  }, [router, searchParams])
+
+    handleConfirmEmail()
+  }, [params, router])
 }
