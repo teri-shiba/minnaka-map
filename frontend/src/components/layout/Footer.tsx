8811b08eb60fcc014983b21989bb967533555183
Teri <erk.nishimura@gmail.com>
2025-06-17 16:44:54 +0900

    refactor

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Image from 'next/image'
 import Link from 'next/link'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import logo from '~/public/logo.webp'
 
 interface NavItemProps {
