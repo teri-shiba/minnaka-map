6e81e48b5a96475e7a39dc257e775e90d5d11792
Teri <erk.nishimura@gmail.com>
2025-06-25 16:03:36 +0900

    add: APIキーをサーバーコンポーネントから渡す

--- frontend/src/components/ui/map/Map.tsx
+++ frontend/src/components/ui/map/Map.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import type { MapProps } from '~/types/map'
-import { useEffect, useState } from 'react'
 import { MapContainer, ZoomControl } from 'react-leaflet'
 import { createLeafletOptions } from '~/utils/createLeafletOptions'
 import MapTailerLayer from './layers/MapTailerLayer'
@@ -10,39 +9,22 @@ import 'leaflet/dist/leaflet.css'
 import '@maptiler/sdk/dist/maptiler-sdk.css'
 
 export default function Map({
+  apiKey,
   userLocation,
 }: MapProps) {
-  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL
-  const [mapApiKey, setMapApiKey] = useState<string | null>(null)
-
-  useEffect(() => {
-    const fetchApiKey = async () => {
-      try {
-        const response = await fetch(`${baseURL}/map_tiler/api_key`)
-        const data = await response.json()
-        setMapApiKey(data.api_key)
-      }
-      catch (e) {
-        console.error('APIキーの取得に失敗:', e)
-      }
-    }
-
-    fetchApiKey()
-  }, [baseURL])
-
   const mapOptions = createLeafletOptions(userLocation)
 
   return (
-    <main className="relative size-full flex-1 overflow-hidden md:w-3/5">
+    <main className="relative z-40 size-full flex-1 overflow-hidden md:w-3/5">
       <MapContainer
         center={userLocation}
         {...mapOptions}
         className="absolute size-full"
       >
-        {mapApiKey
+        {apiKey
           && (
             <MapTailerLayer
-              apiKey={mapApiKey}
+              apiKey={apiKey}
             />
           )}
         <ZoomControl position="bottomright" />
