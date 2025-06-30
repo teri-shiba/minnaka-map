959b02c8d62c103ef65d4ea7593395db06fc2281
Teri <erk.nishimura@gmail.com>
2025-06-30 11:31:15 +0900

    refactor

--- frontend/src/app/result/page.tsx
+++ frontend/src/app/result/page.tsx
@@ -1,48 +1,49 @@
 import type { SearchParams } from '~/types/search-params'
 import { redirect } from 'next/navigation'
 import RestaurantsDrawer from '~/components/ui/drawers/RestaurantsDrawer'
-import Map from '~/components/ui/map/Map'
+import { fetchRestaurants } from '~/services/fetch-restaurants'
 import { getApiKey } from '~/services/get-api-key'
+import { parseAndValidateCoordinates } from '~/services/parse-and-validate-coords'
+import { verifyCoordsSignature } from '~/services/verify-coords-signature'
 
 interface ResultPageProps {
   searchParams: SearchParams
 }
 
 export default async function Result({ searchParams }: ResultPageProps) {
+  const maptilerApiKey = await getApiKey('maptiler')
   const params = await searchParams
 
   if (!params.lat || !params.lng || !params.signature) {
     redirect('/?error=missing_params')
   }
 
-  // const { lat, lng } = await parseAndValidateCoordinates(params)
+  const { lat, lng } = await parseAndValidateCoordinates(params)
 
-  // const userLocation = await verifyCoordsSignature({
-  //   latitude: lat,
-  //   longitude: lng,
-  //   signature: params.signature,
-  //   expires_at: params.expires_at,
-  // })
+  const userLocation = await verifyCoordsSignature({
+    latitude: lat,
+    longitude: lng,
+    signature: params.signature,
+    expires_at: params.expires_at,
+  })
 
-  // const restaurants = await fetchRestaurants({
-  //   latitude: lat,
-  //   longitude: lng,
-  //   radius: params.radius,
-  // })
-
-  const maptilerApiKey = await getApiKey('maptiler')
+  const restaurants = await fetchRestaurants({
+    latitude: lat,
+    longitude: lng,
+    radius: params.radius,
+  })
 
   return (
     <>
       <div className="relative mx-auto h-[calc(100dvh-4rem)] max-w-screen-2xl overflow-hidden sm:flex">
-        <div className="h-[calc(60vh-4rem)] w-full md:h-[calc(100vh-4rem)] md:w-3/5">
-          {(maptilerApiKey && userLocation)
+        <div className="h-[calc(60vh-4rem)] w-full bg-orange-600 md:h-[calc(100vh-4rem)] md:w-3/5">
+          {/* {(maptilerApiKey && userLocation)
             && (
               <Map
                 apiKey={maptilerApiKey}
                 userLocation={userLocation}
               />
-            )}
+            )} */}
         </div>
 
         {/* PC */}
