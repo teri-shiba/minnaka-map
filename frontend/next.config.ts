2408b7318750b4266a60775302a06e470d79014a
Teri <erk.nishimura@gmail.com>
2025-06-30 10:18:26 +0900

    add: Sentry

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next'
+import { withSentryConfig } from '@sentry/nextjs'
 
 const nextConfig: NextConfig = {
   images: {
@@ -20,4 +21,6 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  silent: true,
+})
