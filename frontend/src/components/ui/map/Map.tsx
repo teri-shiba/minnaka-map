30b72de076f832b79aff92c6437b5ed8e80354b9
Teri <erk.nishimura@gmail.com>
2025-07-01 11:18:20 +0900

    refactor: Railsに命名を合わせる userLocation -> midpoint

--- frontend/src/components/ui/map/Map.tsx
+++ frontend/src/components/ui/map/Map.tsx
@@ -4,20 +4,20 @@ import type { MapProps } from '~/types/map'
 import { MapContainer, ZoomControl } from 'react-leaflet'
 import { createLeafletOptions } from '~/utils/create-leaflet-options'
 import MapTailerLayer from './layers/MapTailerLayer'
-import UserLocationMarker from './markers/UserLocationMarker'
+import MidpointMarker from './markers/MidPointMarker'
 import 'leaflet/dist/leaflet.css'
 import '@maptiler/sdk/dist/maptiler-sdk.css'
 
 export default function Map({
   apiKey,
-  userLocation,
+  midpoint,
 }: MapProps) {
-  const mapOptions = createLeafletOptions(userLocation)
+  const mapOptions = createLeafletOptions(midpoint)
 
   return (
     <main className="relative z-40 size-full flex-1 overflow-hidden md:w-3/5">
       <MapContainer
-        center={userLocation}
+        center={midpoint}
         {...mapOptions}
         className="absolute size-full"
       >
@@ -28,7 +28,7 @@ export default function Map({
             />
           )}
         <ZoomControl position="bottomright" />
-        <UserLocationMarker position={userLocation} />
+        <MidpointMarker position={midpoint} />
       </MapContainer>
     </main>
   )
