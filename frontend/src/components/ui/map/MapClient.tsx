d218af35cbd7ab23b5565f7b2a3b411e0d421dc9
Teri <erk.nishimura@gmail.com>
2025-07-08 11:42:12 +0900

    refactor

--- frontend/src/components/ui/map/MapClient.tsx
+++ frontend/src/components/ui/map/MapClient.tsx
@@ -2,11 +2,22 @@
 
 import type { MapItems } from '~/types/map'
 import dynamic from 'next/dynamic'
+import Loading from '~/public/figure_loading_circle.svg'
 import { Skeleton } from '../skeleton/Skeleton'
 
 const Map = dynamic(() => import('./Map'), {
   ssr: false,
-  loading: () => <Skeleton className="h-[calc(60vh-4rem)] w-full md:h-[calc(100vh-4rem)]" />,
+  loading: () => (
+    <div className="relative h-[calc(60vh-4rem)] w-full md:h-[calc(100vh-4rem)]">
+      <Skeleton className="size-full" />
+      <Loading
+        aria-label="ローディング中"
+        width={40}
+        height={40}
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
+    </div>
+  ),
 })
 
 export default function MapClient({
