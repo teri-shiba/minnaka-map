9eb985b3ca0f86a30d4ac2c5399f9384157c7620
Teri <erk.nishimura@gmail.com>
2025-07-15 11:19:01 +0900

    add: 地図上に表示する飲食店カードを新規実装

--- frontend/src/components/features/map/Map.tsx
+++ frontend/src/components/features/map/Map.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import type { MapItems } from '~/types/map'
+import type { RestaurantListItem } from '~/types/restaurant'
+import { useState } from 'react'
 import { MapContainer, ZoomControl } from 'react-leaflet'
 import { createLeafletOptions } from '~/utils/create-leaflet-options'
 import MapTilerLayer from './layers/MapTilerLayer'
+import MapRestaurantCard from './MapRestaurantCard'
 import MidpointMarker from './markers/MidpointMarker'
 import RestaurantMarker from './markers/RestaurantMarker'
 import 'leaflet/dist/leaflet.css'
@@ -14,6 +17,7 @@ export default function Map({
   midpoint,
   restaurants,
 }: MapItems) {
+  const [selectedRestaurant, setSelectedRestaurant] = useState<RestaurantListItem | null>(null)
   const mapOptions = createLeafletOptions(midpoint)
 
   return (
@@ -29,10 +33,22 @@ export default function Map({
               apiKey={apiKey}
             />
           )}
-        <ZoomControl position="bottomright" />
+        <ZoomControl position="topright" />
         <MidpointMarker position={midpoint} />
-        <RestaurantMarker restaurants={restaurants} />
+        <RestaurantMarker
+          restaurants={restaurants}
+          onRestaurantClick={setSelectedRestaurant}
+        />
       </MapContainer>
+
+      {selectedRestaurant && (
+        <div className="absolute inset-x-0 mx-auto bottom-8 z-[999] w-11/12 max-w-96 overflow-hidden rounded-2xl  md:right-10 md:w-60">
+          <MapRestaurantCard
+            restaurant={selectedRestaurant}
+            onClose={() => setSelectedRestaurant(null)}
+          />
+        </div>
+      )}
     </main>
   )
 }
