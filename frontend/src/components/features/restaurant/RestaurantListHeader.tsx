26e756739a4d8bbfab99a1b31eecd772d504904d
Teri <erk.nishimura@gmail.com>
2025-07-17 13:54:02 +0900

    add: SPではバッジセレクトに変更

--- frontend/src/components/features/restaurant/RestaurantListHeader.tsx
+++ frontend/src/components/features/restaurant/RestaurantListHeader.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
+import { Button } from '~/components/ui/buttons/Button'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '~/components/ui/selects/Select'
 import { SORT_GENRE } from '~/constants'
 
@@ -39,25 +40,58 @@ export default function RestaurantListHeader({ totalCount }: RestaurantListHeade
   }
 
   return (
-    <div className="flex flex-wrap items-center justify-between">
+    <div className="flex flex-col-reverse md:flex-row md:items-center md:justify-between">
       <h2 className="text-base">
         検索結果 全
         {totalCount}
         件
       </h2>
-      <Select value={validCurrentGenre} onValueChange={onGenreChange}>
-        <SelectTrigger className="md:w-40">
-          <SelectValue placeholder="料理ジャンル" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="all">すべて</SelectItem>
+
+      {/* PC: Select */}
+      <div className="hidden md:inline-block">
+        <Select value={validCurrentGenre} onValueChange={onGenreChange}>
+          <SelectTrigger className="md:w-52">
+            <SelectValue placeholder="料理ジャンル" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">すべて</SelectItem>
+            {SORT_GENRE.map(genre => (
+              <SelectItem key={genre.code} value={genre.code}>
+                {genre.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+
+      {/* SP: Bottun */}
+      <div
+        className="hidden-scrollbar mb-4 block w-full overflow-x-scroll border-b py-4 md:hidden"
+        style={{ touchAction: 'pan-x' }}
+        onTouchStart={e => e.stopPropagation()}
+        onTouchMove={e => e.stopPropagation()}
+        onTouchEnd={e => e.stopPropagation()}
+      >
+        <div className="flex w-full min-w-max gap-2">
+          <Button
+            variant={!validCurrentGenre ? 'default' : 'outline'}
+            onClick={() => onGenreChange('all')}
+            className="whitespace-nowrap"
+          >
+            すべて
+          </Button>
           {SORT_GENRE.map(genre => (
-            <SelectItem key={genre.code} value={genre.code}>
+            <Button
+              key={genre.code}
+              variant={validCurrentGenre === genre.code ? 'default' : 'outline'}
+              onClick={() => onGenreChange(genre.code)}
+              className="whitespace-nowrap"
+            >
               {genre.name}
-            </SelectItem>
+            </Button>
           ))}
-        </SelectContent>
-      </Select>
+        </div>
+      </div>
     </div>
   )
 }
