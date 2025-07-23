f92282697e831851aa7767cccc175c7af00f3360
teri-shiba <erk.nishimura@gmail.com>
2025-07-23 09:49:29 +0900

    add: uuid を扱うため 'uuid-ossp' を追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_07_013422) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_23_002419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "operators", force: :cascade do |t|
     t.string "name", null: false
