20ad60304dd5c35bdf825a3e4514b762905e41fb
teri-shiba <erk.nishimura@gmail.com>
2025-07-25 14:40:48 +0900

    fix: お気に入りテーブルと紐付け

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :user_auth, dependent: :destroy
+  has_many :search_histories, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
   validates :name, presence: true
+
+  def favorites_by_search_history
+    favorites.includes(:search_history).joins(:search_history).group_by(&:search_history)
+  end
 end
