517bddb5eb20fe66d43d444b2628b2cef07e408e
teri-shiba <erk.nishimura@gmail.com>
2025-07-28 17:56:08 +0900

    delete: 不要なので削除

--- backend/app/models/location.rb
+++
@@ -1,6 +0,0 @@
-class Location < ApplicationRecord
-  belongs_to :prefecture
-  has_one :station, dependent: :destroy
-  has_many :search_history_start_locations, dependent: :destroy
-  has_many :search_history_center_locations, dependent: :destroy
-end
--- backend/app/models/search_history_start_location.rb
+++
@@ -1,4 +0,0 @@
-class SearchHistoryStartLocation < ApplicationRecord
-  belongs_to :location
-  belongs_to :search_history
-end
