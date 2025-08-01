282198e0a566b376e21af720838df0bf0436ddbf
teri-shiba <erk.nishimura@gmail.com>
2025-08-01 13:45:45 +0900

    fix: ログアウトでトップページに戻る && ページ遷移でメニューを閉じる

--- frontend/src/components/ui/dropdownmenu/UserMenu.tsx
+++ frontend/src/components/ui/dropdownmenu/UserMenu.tsx
@@ -1,28 +1,40 @@
+'use client'
+
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 import { HiUser } from 'react-icons/hi2'
 import { LuHeart, LuLogOut, LuSettings } from 'react-icons/lu'
 import { useAuth } from '~/hooks/useAuth'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './DropdownMenu'
 
 export default function UserMenu() {
+  const [open, setOpen] = useState<boolean>(false)
+  const router = useRouter()
   const { logout } = useAuth()
 
   const handleLogout = async () => {
     await logout()
+    router.push('/')
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger className="flex size-10 items-center justify-center overflow-hidden rounded-full border border-orange-900/10 bg-muted">
-        <HiUser className="size-5 text-primary" />
+    <DropdownMenu open={open} onOpenChange={setOpen}>
+      <DropdownMenuTrigger asChild>
+        <div
+          onClick={() => setOpen(true)}
+          className="flex size-10 cursor-pointer items-center justify-center overflow-hidden rounded-full border bg-muted"
+        >
+          <HiUser className="size-5 text-primary" />
+        </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuItem>
-          <Link href="/favorites" className="flex items-center gap-2">
+        <Link href="/favorites" onClick={() => setOpen(false)} className="block">
+          <DropdownMenuItem className="flex items-center gap-2">
             <LuHeart className="size-4 text-slate-400" />
             お気に入り一覧
-          </Link>
-        </DropdownMenuItem>
+          </DropdownMenuItem>
+        </Link>
         <DropdownMenuItem>
           <LuSettings className="size-4 text-slate-400" />
           {' '}
