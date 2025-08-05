58cdf411ca6963639be57b8a668dec9243180b45
teri-shiba <erk.nishimura@gmail.com>
2025-08-05 13:34:41 +0900

    delete

--- backend/spec/factories/search_history_center_locations.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :search_history_center_location do
-    searh_history { nil }
-    location { nil }
-  end
-end
--- backend/spec/factories/search_history_start_locations.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :search_history_start_location do
-    location { nil }
-    search_history { nil }
-  end
-end
