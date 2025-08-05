f1bde821f124d0d4ba8378c2f5057106bddc150d
teri-shiba <erk.nishimura@gmail.com>
2025-08-05 15:58:10 +0900

    add: SearchHistoryStartStation モデルのテストを追加

--- backend/spec/models/search_history_start_station_spec.rb
+++ backend/spec/models/search_history_start_station_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe SearchHistoryStartStation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "関連付け" do
+    it { should belong_to(:search_history) }
+    it { should belong_to(:station) }
+  end
+
+  describe "ファクトリ" do
+    it "有効なファクトリを持つこと" do
+      search_history_start_station = build(:search_history_start_station)
+      expect(search_history_start_station).to be_valid
+    end
+
+    it "search_history と station が正しく関連づけられること" do
+      search_history_start_station = create(:search_history_start_station)
+
+      expect(search_history_start_station.search_history).to be_valid
+      expect(search_history_start_station.station).to be_valid
+    end
+  end
 end
