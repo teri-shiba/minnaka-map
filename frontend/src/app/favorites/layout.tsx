b1409086effee126ea4819bd1e828157da7f7440
teri-shiba <erk.nishimura@gmail.com>
2025-08-20 16:11:49 +0900

    add: メニューとページタイトルを統一

--- frontend/src/app/favorites/layout.tsx
+++ frontend/src/app/favorites/layout.tsx
@@ -12,7 +12,7 @@ export default async function FavoritesLayout({ children }: { children: React.Re
   return (
     <>
       <header className="flex h-32 flex-col items-center justify-center bg-secondary md:h-48">
-        <h1 className="text-2xl font-bold">お気に入りリスト</h1>
+        <h1 className="text-2xl font-bold">お気に入り一覧</h1>
       </header>
 
       <Section className="py-8 md:py-10">
