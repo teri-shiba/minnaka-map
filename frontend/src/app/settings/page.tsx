358dc78cdb8128f83f1e405606a31e95d5e2cbcf
teri-shiba <erk.nishimura@gmail.com>
2025-08-25 15:08:42 +0900

    refactor: CSSを調整

--- frontend/src/app/settings/page.tsx
+++ frontend/src/app/settings/page.tsx
@@ -1,3 +1,4 @@
+import Section from '~/components/layout/Section'
 import { DeleteAccount } from '~/components/ui/DeleteAccount'
 
 export default function SettingsPage() {
@@ -6,13 +7,15 @@ export default function SettingsPage() {
       <header className="flex h-32 flex-col items-center justify-center bg-secondary md:h-48">
         <h1 className="px-5 text-lg font-bold sm:text-2xl">アカウント設定</h1>
       </header>
-      <div className="mx-auto max-w-lg py-8 md:flex md:items-center md:justify-between md:py-10">
-        <div>
-          <p className="py-1 text-lg font-bold">アカウント削除</p>
-          <p className="text-sm">削除すると復元はできません。</p>
+      <Section className="py-8 md:py-10">
+        <div className="mx-auto flex max-w-lg justify-between md:items-center">
+          <div>
+            <p className="py-1 text-base font-bold md:text-lg">アカウント削除</p>
+            <p className="text-xs md:text-sm">削除すると復元はできません。</p>
+          </div>
+          <DeleteAccount />
         </div>
-        <DeleteAccount />
-      </div>
+      </Section>
     </>
   )
 }
