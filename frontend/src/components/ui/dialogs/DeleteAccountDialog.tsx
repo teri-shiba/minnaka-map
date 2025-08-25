bf949579697ad5b67310ebad1d79d556fd9e1282
teri-shiba <erk.nishimura@gmail.com>
2025-08-25 14:05:56 +0900

    add: get-delete-description.ts を追加

--- frontend/src/components/ui/dialogs/DeleteAccountDialog.tsx
+++ frontend/src/components/ui/dialogs/DeleteAccountDialog.tsx
@@ -1,37 +1,37 @@
 'use client'
 
+import { useAtomValue } from 'jotai'
 import { useState } from 'react'
+import { userStateAtom } from '~/state/user-state.atom'
+import { getDeleteDescription } from '~/utils/get-delete-description'
 import { Button } from '../buttons/Button'
-
 import DeleteAccountForm from '../forms/DeleteAccountForm'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from './Dialog'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './Dialog'
 
 export function DeleteAccountDialog() {
   const [open, setOpen] = useState(false)
+  const user = useAtomValue(userStateAtom)
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button variant="destructive" className="border border-destructive bg-white text-destructive">退会する</Button>
+        <Button
+          className="border border-destructive bg-white text-destructive hover:bg-destructive hover:text-white"
+        >
+          削除する
+        </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[470px]">
         <DialogHeader className="flex flex-col space-y-3">
           <DialogTitle className="text-center">
             アカウント削除
           </DialogTitle>
-          <DialogDescription>
-            この操作は戻すことができません。
+          <DialogDescription className="leading-6 text-foreground">
+            {getDeleteDescription(user.provider)}
             <br />
-            確認のため、登録中のメールアドレスを入力してください。
+            この操作は戻すことができません。
           </DialogDescription>
         </DialogHeader>
-        <DeleteAccountForm />
+        <DeleteAccountForm onClose={() => setOpen(false)} />
       </DialogContent>
     </Dialog>
   )
