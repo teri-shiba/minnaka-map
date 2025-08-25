0f7c123882fb2bea347cca498497e237c2f52ab5
teri-shiba <erk.nishimura@gmail.com>
2025-08-25 14:05:56 +0900

    add: get-delete-description.ts を追加

--- frontend/src/components/ui/drawers/DeleteAccountDrawer.tsx
+++ frontend/src/components/ui/drawers/DeleteAccountDrawer.tsx
@@ -1,41 +1,39 @@
 'use client'
 
+import { useAtomValue } from 'jotai'
 import { useState } from 'react'
+import { userStateAtom } from '~/state/user-state.atom'
+import { getDeleteDescription } from '~/utils/get-delete-description'
 import { Button } from '../buttons/Button'
-
-import {
-  DialogDescription,
-} from '../dialogs/Dialog'
-
+import { DialogDescription } from '../dialogs/Dialog'
 import DeleteAccountForm from '../forms/DeleteAccountForm'
-import {
-  Drawer,
-  DrawerContent,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from './Drawer'
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from './Drawer'
 
 export function DeleteAccountDrawer() {
   const [open, setOpen] = useState(false)
+  const user = useAtomValue(userStateAtom)
 
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
-        <Button variant="destructive" className="border border-destructive bg-white text-destructive">退会する</Button>
+        <Button
+          className="border border-destructive bg-white text-destructive hover:bg-destructive hover:text-white"
+        >
+          削除する
+        </Button>
       </DrawerTrigger>
       <DrawerContent className="mb-8">
         <DrawerHeader className="flex flex-col space-y-2">
           <DrawerTitle className="text-center">
             アカウント削除
           </DrawerTitle>
-          <DialogDescription className="text-left">
-            この操作は戻すことができません。
+          <DialogDescription className="text-left text-foreground">
+            {getDeleteDescription(user.provider)}
             <br />
-            確認のため、登録中のメールアドレスを入力してください。
+            この操作は戻すことができません。
           </DialogDescription>
         </DrawerHeader>
-        <DeleteAccountForm />
+        <DeleteAccountForm onClose={() => setOpen(false)} />
       </DrawerContent>
     </Drawer>
   )
