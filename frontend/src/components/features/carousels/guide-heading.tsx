7e32a6ad4235fff10dd32f00fe68c91fa6468263
teri-shiba <erk.nishimura@gmail.com>
2025-09-01 13:40:51 +0900

    refactor: aria-label が不要なので削除

--- frontend/src/components/features/carousels/guide-heading.tsx
+++ frontend/src/components/features/carousels/guide-heading.tsx
@@ -6,7 +6,6 @@ export default function GuideHeading() {
       <h2 className="flex flex-wrap items-center justify-center gap-1 px-5 pb-4 text-lg text-secondary-foreground md:row-span-1 md:flex-nowrap md:justify-start md:pb-0 md:pl-5 md:text-2xl">
         <Image
           alt="minnaka map"
-          aria-label="minnaka map"
           src="/logo.webp"
           width={240}
           height={30}
