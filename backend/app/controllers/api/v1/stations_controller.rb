23f361f5805496cd7a9f78ad330d01a0538b2f40
teri-shiba <erk.nishimura@gmail.com>
2025-09-04 19:02:59 +0900

    refactor: station_response をシリアライザーに委譲する

--- backend/app/controllers/api/v1/stations_controller.rb
+++ backend/app/controllers/api/v1/stations_controller.rb
@@ -5,18 +5,9 @@ class Api::V1::StationsController < ApplicationController
 
     stations = Station.search_by_name(query).limit(5)
     render json: {
-      stations: stations.map {|s| station_response(s) },
+      stations: ActiveModelSerializers::SerializableResource.new(
+        stations, each_serializer: StationIndexSerializer
+      ).as_json,
     }
   end
-
-  private
-
-    def station_response(station)
-      {
-        id: station.id,
-        name: station.display_name,
-        latitude: station.latitude,
-        longitude: station.longitude,
-      }
-    end
 end
