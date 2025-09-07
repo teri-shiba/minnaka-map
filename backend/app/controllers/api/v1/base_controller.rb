d294cb63a4ea67ff6c1cb294c5c38b33b12207da
teri-shiba <erk.nishimura@gmail.com>
2025-09-07 16:08:43 +0900

    refactor: ヘルパーの継承を application_controller に委譲

--- backend/app/controllers/api/v1/base_controller.rb
+++ backend/app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,4 @@
 class Api::V1::BaseController < ApplicationController
-  include Api::ResponseHelper
-  include Api::ExceptionHandler
-
   alias_method :current_user, :current_api_v1_user_auth
   alias_method :authenticate_user!, :authenticate_api_v1_user_auth!
   alias_method :user_signed_in?, :api_v1_user_auth_signed_in?
