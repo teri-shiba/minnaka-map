e9d3eae6bd388f96f9c3273bde4be03a3c9ac61a
teri-shiba <erk.nishimura@gmail.com>
2025-09-08 19:02:31 +0900

    fix: `render_error()` が device token auth と衝突するので、ラッパーを実装し衝突回避

--- backend/app/controllers/api/v1/overrides/registrations_controller.rb
+++ backend/app/controllers/api/v1/overrides/registrations_controller.rb
@@ -1,23 +1,31 @@
 class Api::V1::Overrides::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  include Api::ResponseHelperWrapper
+
+  def create
+    if sign_up_params[:name].blank?
+      render_api_error("Name can't be blank", status: :unprocessable_entity)
+      return
+    end
+    super
+  end
+
   def build_resource
-    user_auth_params = sign_up_params.except(:name)
+    auth_params = sign_up_params.except(:name)
 
-    @resource = resource_class.new(user_auth_params)
+    @resource = resource_class.new(auth_params)
     @resource.provider = provider
-
-    @resource.email = if resource_class.case_insensitive_keys.include?(:email)
-                        user_auth_params[:email]&.downcase
-                      else
-                        user_auth_params[:email]
-                      end
-
-    user = User.create!(name: sign_up_params[:name])
-    @resource.user_id = user.id
+    @resource.email =
+      if resource_class.case_insensitive_keys.include?(:email)
+        auth_params[:email].to_s.downcase
+      else
+        auth_params[:email]
+      end
+    @resource.build_user(name: sign_up_params[:name])
   end
 
   private
 
     def sign_up_params
-      params.permit(:email, :password, :password_confirmation, :name, :user_id)
+      params.permit(:email, :password, :password_confirmation, :name)
     end
 end
