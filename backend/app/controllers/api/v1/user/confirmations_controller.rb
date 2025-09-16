6a541676bccbfc2777c405c728bd7a4310548f3c
teri-shiba <erk.nishimura@gmail.com>
2025-09-16 17:25:21 +0900

    refactor: 日本語のメッセージに対応

--- backend/app/controllers/api/v1/user/confirmations_controller.rb
+++ backend/app/controllers/api/v1/user/confirmations_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::User::ConfirmationsController < Api::V1::BaseController
   def update
     user = UserAuth.find_by(confirmation_token: params[:confirmation_token])
-    return render json: { message: "User record is not found." }, status: :not_found if user.nil?
-    return render json: { message: "User has already been confirmed." }, status: :bad_request if user.confirmed?
+
+    if user.nil?
+      message = I18n.t("activerecord.models.user") + I18n.t("errors.messages.not_found")
+      return render json: { message: }, status: :not_found
+    end
+
+    if user.confirmed?
+      message = I18n.t("activerecord.attributes.user_auth.email") + I18n.t("errors.messages.already_confirmed")
+      return render json: { message: }, status: :bad_request
+    end
 
     user.update!(confirmed_at: Time.current)
-
-    render json: { message: "User confirmation succeeded." }, status: :ok
+    render json: { message: I18n.t("devise.confirmations.confirmed") }, status: :ok
   end
 end
