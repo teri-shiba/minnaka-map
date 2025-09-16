657b9072bb33f9e5619629dcefba658d2d872793
teri-shiba <erk.nishimura@gmail.com>
2025-09-16 16:46:01 +0900

    refactor: マッチャを拡張

--- backend/spec/support/matchers/uuid_matcher.rb
+++ backend/spec/support/matchers/uuid_matcher.rb
@@ -1,3 +1,15 @@
+UUID_REGEX = /\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/i
+
 RSpec::Matchers.define :be_uuid do
-  match {|actual| actual.to_s.match?(/\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i) }
+  match {|actual| actual.to_s.match?(UUID_REGEX) }
+  failure_message {|actual| "expected #{actual.inspect} to match UUID format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX" }
+  description { "match a generic UUID (any version)" }
+end
+
+UUID_V4_REGEX = /\A\h{8}-\h{4}-4\h{3}-[89abAB]\h{3}-\h{12}\z/
+
+RSpec::Matchers.define :be_uuid_v4 do
+  match {|actual| actual.to_s.match?(UUID_V4_REGEX) }
+  failure_message {|actual| "expected #{actual.inspect} to match UUID v4 format" }
+  description { "match a UUID v4" }
 end
