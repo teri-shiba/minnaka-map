b23587392556ed006b0f08a6bce9a4bbba31af22
teri-shiba <erk.nishimura@gmail.com>
2025-09-16 13:51:42 +0900

    refactor: error の拡張

--- backend/spec/support/api_response_helpers.rb
+++ backend/spec/support/api_response_helpers.rb
@@ -1,40 +1,68 @@
 module ApiResponseHelpers
+  # --- basic ---
   def json
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def data
-    json.fetch(:data)
-  end
+  def data = json.fetch(:data)
+  def meta = json.fetch(:meta)
+  def error = json.fetch(:error)
 
-  def meta
-    json.fetch(:meta)
-  end
-
-  def error
-    json.fetch(:error)
-  end
-
+  # --- success ---
   def expect_success_json!(data: nil, status: :ok)
     expect(response).to have_http_status(status)
     expect(json[:success]).to be(true)
-    expect(json[:data]).to eq(data) if data
+    expect(json[:data]).to eq(data) unless data.nil?
   end
 
   def expect_status_ok! = expect_success_json!(status: :ok)
   def expect_status_created! = expect_success_json!(status: :created)
 
-  def expect_unprocessable_json!(message: nil, details: nil)
-    expect(response).to have_http_status(:unprocessable_entity)
-    expect(json[:success]).to be(false)
-    expect(json.dig(:error, :message)).to eq(message) if message
-    expect(json.dig(:error, :details)).to eq(details) if details
+  # --- error ---
+  def expect_error_json!(status:, message: nil, details: nil)
+    expect(response).to have_http_status(status)
+    payload = json
+
+    if payload.has_key?(:error)
+      assert_wrapper_error!(payload, message, details)
+    elsif payload.has_key?(:errors)
+      assert_dta_error!(payload, message)
+    end
   end
 
   def expect_bad_request_json!(message: nil, details: nil)
-    expect(response).to have_http_status(:bad_request)
-    expect(json[:success]).to be(false) if json.has_key?(:success)
-    expect(json.dig(:error, :message)).to eq(message) if message && json.has_key?(:error)
-    expect(json.dig(:error, :details)).to eq(details) if details && json.has_key?(:error)
+    expect_error_json!(status: :bad_request, message:, details:)
   end
+
+  def expect_unprocessable_json!(message: nil, details: nil)
+    expect_error_json!(status: :unprocessable_entity, message:, details:)
+  end
+
+  def expect_not_found_json!(message: nil, details: nil)
+    expect_error_json!(status: :not_found, message:, details:)
+  end
+
+  def expect_unauthorized_json!(message: "ログインもしくはアカウント登録してください")
+    expect_error_json!(status: :unauthorized, message:)
+  end
+
+  private
+
+    def assert_wrapper_error!(payload, message, details)
+      expect(payload[:success]).to be(false) if payload.has_key?(:success)
+      expect(payload.dig(:error, :message)).to eq(message) if message
+      expect(payload.dig(:error, :details)).to eq(details) if details
+    end
+
+    def assert_dta_error!(payload, message)
+      return unless message
+
+      errors = payload[:errors]
+
+      if errors.is_a?(Array)
+        expect(errors).to include(message)
+      else
+        expect(errors).to eq(message)
+      end
+    end
 end
