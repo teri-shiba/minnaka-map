3a4541ccd87041bc4ff88e9eae28d329af3856cc
teri-shiba <erk.nishimura@gmail.com>
2025-09-17 18:18:23 +0900

    refactor: resource_class と resource_name が UserAuth であることを担保 && @resource を途中で上書きしていた記述を変更  && 整形

--- backend/app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
+++ backend/app/controllers/api/v1/auth/omniauth_callbacks_controller.rb
@@ -1,78 +1,72 @@
 class Api::V1::Auth::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
   def omniauth_success
-    logger.debug "OmniAuth Auth Hash (LINE): #{auth_hash.inspect}"
     return if prevent_duplicate_email_redirect
 
     resource_from_auth_hash
     set_token_on_resource
     create_auth_params
 
-    if confirmable_enabled?
-      @resource.skip_confirmation!
-    end
-
-    sign_in(:user, @resource, store: false, bypass: false)
+    @resource.skip_confirmation! if confirmable_enabled?
 
     @resource.save!
 
+    sign_in(resource_name, @resource, store: false, bypass: false)
+
     yield @resource if block_given?
 
-    if DeviseTokenAuth.cookie_enabled
-      set_token_in_cookie(@resource, @token)
-    end
+    set_token_in_cookie(@resource, @token) if DeviseTokenAuth.cookie_enabled
 
-    redirect_to "#{Settings.front_domain}/?status=success"
+    redirect_to "#{Settings.front_domain}/?status=success", allow_other_host: true
   end
 
   def omniauth_failure
-    redirect_to "#{Settings.front_domain}/?status=error"
+    redirect_to "#{Settings.front_domain}/?status=error", allow_other_host: true
   end
 
   private
 
+    def resource_class = UserAuth
+    def resource_name = :user_auth
+
     def prevent_duplicate_email_redirect
-      email = auth_hash["info"]["email"]
+      email = auth_hash.dig("info", "email")
       provider = auth_hash["provider"]
       uid = auth_hash["uid"]
 
       return false unless email && provider && uid
 
-      is_new_sns_user = !resource_class.exists?(provider: provider, uid: uid)
+      new_sns_user = !resource_class.exists?(provider: provider, uid: uid)
 
       email_exists = resource_class.exists?(email: email)
 
-      if is_new_sns_user && email_exists
-        error_message = "このメールアドレスは既に登録されています"
-        redirect_to "#{Settings.front_domain}/?status=error&message=#{CGI.escape(error_message)}"
+      if new_sns_user && email_exists
+        attr = UserAuth.human_attribute_name(:email)
+        suffix = I18n.t("activerecord.errors.models.user_auth.attributes.email.taken")
+        message = "#{attr}#{suffix}"
+        query = { status: "error", message: }.to_query
+        redirect_to "#{Settings.front_domain}/?#{query}", allow_other_host: true
         return true
       end
       false
     end
 
     def resource_from_auth_hash
-      @resource = resource_class.where(
+      @resource = resource_class.find_or_initialize_by(
         uid: auth_hash["uid"],
         provider: auth_hash["provider"],
-      ).first_or_initialize
+      )
 
       if @resource.new_record?
         @oauth_registration = true
-
-        user = User.new(name: auth_hash["info"]["name"])
-        user.save!
-        @resource.user = user
-
-        @resource = UserAuth.new(
-          provider: auth_hash["provider"],
-          uid: auth_hash["uid"],
-          email: auth_hash["info"]["email"],
+        user = User.create!(name: auth_hash.dig("info", "name") || "unknown")
+        @resource.assign_attributes(
+          email: auth_hash.dig("info", "email"),
           password: set_random_password,
           user: user,
         )
       end
 
       assign_provider_attrs(@resource, auth_hash)
-
       @resource
     end
 end
