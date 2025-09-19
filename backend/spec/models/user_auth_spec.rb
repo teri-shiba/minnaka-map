4c995b855ee619ef686aa502139673d9a8a0b8bc
teri-shiba <erk.nishimura@gmail.com>
2025-09-19 17:18:27 +0900

    test: add_permitted_params を追加

--- backend/spec/models/user_auth_spec.rb
+++ backend/spec/models/user_auth_spec.rb
@@ -1,25 +1,27 @@
 require "rails_helper"
 
 RSpec.describe UserAuth, type: :model do
-  context "FactoryBot のデフォルト値のまま" do
+  describe "associations" do
+    it { is_expected.to belong_to(:user).required }
+  end
+
+  describe "factory" do
     let(:user_auth) { create(:user_auth) }
 
-    it "認証済みの user_auth レコードを正常に新規作成できること" do
-      expect(user_auth).to be_valid
-      expect(user_auth).to be_confirmed
+    it "有効かつ確認済みであること" do
+      aggregate_failures do
+        expect(user_auth).to be_valid
+        expect(user_auth).to be_confirmed
+      end
     end
   end
 
-  describe "バリデーション" do
-    it "user が必須であること" do
-      record = build(:user_auth, user: nil)
-      expect(record).to be_invalid
-      expect(record.errors[:user]).to be_present
-    end
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:user) }
   end
 
-  describe "DB 制約" do
-    it "user_id を nil にできない（NOT NULL 制約が効く）" do
+  describe "database constraints" do
+    it "user_id を nil にできない（NOT NULL 制約）" do
       record = create(:user_auth)
 
       sql = ActiveRecord::Base.send(
@@ -30,6 +32,14 @@ RSpec.describe UserAuth, type: :model do
       expect {
         ActiveRecord::Base.connection.exec_update(sql, "db_constraint_test")
       }.to raise_error(ActiveRecord::NotNullViolation)
+    end
+  end
+
+  describe ".add_permitted_params" do
+    it "Devise parameter sanitizer に追加する許可リスト (:nameのみ)を返すこと" do
+      permitted = UserAuth.add_permitted_params
+      expect(permitted).to contain_exactly(:name)
+      expect(permitted).to all(be_a(Symbol))
     end
   end
 end
