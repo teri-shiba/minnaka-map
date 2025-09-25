670eddb05be6782c8e17a3c5aad8626f3f0c18cc
teri-shiba <erk.nishimura@gmail.com>
2025-09-25 17:39:57 +0900

    fix: 確認用パスワードが空のときは、入力メッセージのみを表示

--- frontend/src/schemas/signup.schema.ts
+++ frontend/src/schemas/signup.schema.ts
@@ -22,9 +22,12 @@ export const signupSchema = z
       .string()
       .min(1, { message: '確認用のパスワードを入力してください' }),
   })
-  .superRefine(({ password, password_confirmation }, ctx) => {
+  .superRefine(({ password, password_confirmation }, context) => {
+    if (password_confirmation.length === 0)
+      return
+
     if (password !== password_confirmation) {
-      ctx.addIssue({
+      context.addIssue({
         path: ['password_confirmation'],
         code: z.ZodIssueCode.custom,
         message: 'パスワードが一致しません',
