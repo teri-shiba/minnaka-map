a85b10f89a37bbdebf3dc6a57c8fee0843bdf209
teri-shiba <erk.nishimura@gmail.com>
2025-10-02 11:46:38 +0900

    fix: zod v3 から v4 の記述に変更

--- frontend/src/schemas/signup.schema.ts
+++ frontend/src/schemas/signup.schema.ts
@@ -5,32 +5,30 @@ export const signupSchema = z
     name: z
       .string()
       .trim()
-      .min(1, { message: '名前を入力してください' }),
+      .min(1, '名前を入力してください'),
     email: z
-      .string()
-      .email(
-        { message: 'メールアドレスの形式が正しくありません' },
-      ),
+      .email('メールアドレスの形式が正しくありません'),
     password: z
       .string()
-      .min(8, { message: 'パスワードは8文字以上で入力してください' })
-      .regex(/\d+/, { message: '数字を1文字以上使用してください' })
-      .regex(/[a-z]+/, { message: '英小文字を1文字以上使用してください' })
-      .regex(/[A-Z]+/, { message: '英大文字を1文字以上使用してください' })
-      .regex(/[+*'"`#$%&\-^\\@;:,./=~|[\](){}<>]+/, { message: '記号を1文字以上使用してください' }),
+      .min(8, 'パスワードは8文字以上で入力してください')
+      .regex(/\d+/, '数字を1文字以上使用してください')
+      .regex(/[a-z]+/, '英小文字を1文字以上使用してください')
+      .regex(/[A-Z]+/, '英大文字を1文字以上使用してください')
+      .regex(/[+*'"`#$%&\-^\\@;:,./=~|[\](){}<>]+/, '記号を1文字以上使用してください'),
     password_confirmation: z
       .string()
-      .min(1, { message: '確認用のパスワードを入力してください' }),
+      .min(1, '確認用のパスワードを入力してください'),
   })
   .superRefine(({ password, password_confirmation }, context) => {
     if (password_confirmation.length === 0)
       return
 
     if (password !== password_confirmation) {
-      context.addIssue({
+      context.issues.push({
+        code: 'custom',
         path: ['password_confirmation'],
-        code: z.ZodIssueCode.custom,
         message: 'パスワードが一致しません',
+        input: password_confirmation,
       })
     }
   })
