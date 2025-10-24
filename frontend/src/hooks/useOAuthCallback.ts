2cc98278d24ed873b5332574ada5f2a71e38a657
teri-shiba <erk.nishimura@gmail.com>
2025-10-24 10:18:15 +0900

    refactor: useOAuthCallback.ts
    - constants の利用廃止
    - 元いたページに戻れるようにリダイレクトパスを指定
    - if..else をやめ、早期リターンで判定
    - logger の内容を変更

--- frontend/src/hooks/useOAuthCallback.ts
+++ frontend/src/hooks/useOAuthCallback.ts
@@ -1,11 +1,11 @@
 'use client'
 
+import { isAxiosError } from 'axios'
 import { useResetAtom } from 'jotai/utils'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 import { toast } from 'sonner'
 import { useSWRConfig } from 'swr'
-import { API_ENDPOINTS } from '~/constants'
 import api from '~/lib/axios-interceptor'
 import { logger } from '~/lib/logger'
 import { userStateAtom } from '~/state/user-state.atom'
@@ -13,53 +13,70 @@ import { userStateAtom } from '~/state/user-state.atom'
 export default function useOAuthCallback() {
   const router = useRouter()
   const searchParams = useSearchParams()
-  const pathname = usePathname()
   const { mutate } = useSWRConfig()
   const resetUser = useResetAtom(userStateAtom)
 
   const status = searchParams.get('status')
+  const redirectTo = searchParams.get('redirect_to') || '/'
   const message = searchParams.get('message')
 
   useEffect(() => {
     if (!status)
       return
 
-    const handleOAuthCallback = async () => {
-      if (status === 'success') {
-        try {
-          await toast.promise(
-            api.get(API_ENDPOINTS.CURRENT_USER_STATUS)
-              .then((response) => {
-                if (response.data.login === false) {
-                  throw new Error('未ログイン')
-                }
-                return response
-              }),
-            {
-              loading: '認証中…',
-              success: 'ログインに成功しました',
-              error: '認証に失敗しました',
-            },
-          )
-          await mutate(API_ENDPOINTS.CURRENT_USER_STATUS)
-        }
-        catch (error) {
-          logger(error, { component: 'handleOAuthCallback' })
-          resetUser()
-          toast.error('ログインの検証に失敗しました')
-        }
-        router.replace(pathname, { scroll: false })
-      }
-      else {
+    const processOAuthCallback = async () => {
+      if (status !== 'success') {
         resetUser()
         const errorMessage = (status === 'error' && message)
-          ? decodeURIComponent(message)
+          ? decodeURIComponent(message) // メール重複判定
           : '不正なアクセスです'
+
         toast.error(errorMessage)
-        router.replace(pathname, { scroll: false })
+
+        const redirectTo = (status === 'error')
+          ? (searchParams.get('redirect_to') || '/')
+          : '/'
+
+        router.replace(redirectTo, { scroll: false })
+        return
+      }
+
+      try {
+        await toast.promise(
+          api.get('/current/user/show_status').then((response) => {
+            if (response.data.login === false)
+              throw new Error('ログインに失敗しました')
+
+            return response
+          }),
+          {
+            loading: '確認中…',
+            success: 'ログインに成功しました',
+          },
+        )
+
+        await mutate('/current/user/show_status')
+      }
+      catch (error) {
+        logger(error, {
+          component: 'useOAuthCallback',
+          action: 'processOAuthCallback',
+        })
+        resetUser()
+
+        if (!isAxiosError(error) && error instanceof Error)
+          toast.error(error.message)
+      }
+      finally {
+        const url = new URL(window.location.href)
+        url.searchParams.delete('status')
+        url.searchParams.delete('redirect_to')
+
+        const cleanPath = url.pathname + (url.search || '')
+        router.replace(cleanPath, { scroll: false })
       }
     }
 
-    handleOAuthCallback()
-  }, [status, message, mutate, resetUser, router, pathname])
+    processOAuthCallback()
+  }, [status, message, mutate, resetUser, router, redirectTo, searchParams])
 }
