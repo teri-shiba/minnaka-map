020a005032f9fb648434b21fadf13d3b0acfc369
teri-shiba <erk.nishimura@gmail.com>
2025-10-24 10:11:29 +0900

    fix: Rubocop エラー解消

--- backend/config/initializers/omniauth.rb
+++ backend/config/initializers/omniauth.rb
@@ -1,35 +1,33 @@
+OMNIAUTH_SETUP_PROC = ->(env) do
+  request = Rack::Request.new(env)
+  redirect_to = request.params["redirect_to"]
+
+  if redirect_to
+    env["omniauth.strategy"].options[:authorize_params] ||= {}
+    env["rack.session"]["omniauth.state"] || SecureRandom.hex(24)
+    env["rack.session"]["omniauth.redirect_to"] = redirect_to
+  end
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   OmniAuth.config.logger = Rails.logger
 
-  setup_proc = ->(env) do
-    request = Rack::Request.new(env)
-    redirect_to = request.params["redirect_to"]
-
-    if redirect_to
-      env["omniauth.strategy"].options[:authorize_params] ||= {}
-
-      env["rack.session"]["omniauth.state"] || SecureRandom.hex(24)
-
-      env["rack.session"]["omniauth.redirect_to"] = redirect_to
-    end
-  end
-
   if Rails.env.test?
     require "omniauth/strategies/line"
-    provider :line, "dummy_id", "dummy_secret", setup: setup_proc
-    provider :google_oauth2, "dummy_id", "dummy_secret", setup: setup_proc
+    provider :line, "dummy_id", "dummy_secret", setup: OMNIAUTH_SETUP_PROC
+    provider :google_oauth2, "dummy_id", "dummy_secret", setup: OMNIAUTH_SETUP_PROC
   else
     provider :google_oauth2,
              Rails.application.credentials.google[:client_id],
              Rails.application.credentials.google[:client_secret],
-             setup: setup_proc
+             setup: OMNIAUTH_SETUP_PROC
 
     require "omniauth/strategies/line"
     provider :line,
              Rails.application.credentials.line[:channel_id],
              Rails.application.credentials.line[:channel_secret],
              scope: "profile openid email",
-             setup: setup_proc
+             setup: OMNIAUTH_SETUP_PROC
   end
 
   OmniAuth.config.allowed_request_methods = %i[get]
