67a93a57cc97e3315822ae615a408befe1afbc8c
teri-shiba <erk.nishimura@gmail.com>
2025-11-14 14:50:44 +0900

    fix: session_controller は上書き不要なので関連ファイルを削除

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for "UserAuth", at: "auth", controllers: {
         omniauth_callbacks: "api/v1/auth/omniauth_callbacks",
         registrations: "api/v1/overrides/registrations",
-        sessions: "api/v1/auth/sessions",
       }
 
       resources :stations, only: [:index]
