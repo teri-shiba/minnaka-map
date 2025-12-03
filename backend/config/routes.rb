639434be2b1034b536392f3a13fc61d729524b04
teri-shiba <erk.nishimura@gmail.com>
2025-12-03 14:55:33 +0900

    refactor: Rubocop

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
       ['{"status":"ok"}'],
     ]
   }
-  
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   namespace :api do
     namespace :v1 do
