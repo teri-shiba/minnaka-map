1fda2564ab251ba31d2bfdca8429dbdbaf5b9a82
teri-shiba <erk.nishimura@gmail.com>
2025-12-18 11:26:21 +0900

    add: 本番環境のメール送信設定を追加（Amazon SES）
    - production.rb に SMTP 設定を追加
    - devise.rb の送信元アドレスを変更
    - credentials に認証情報を追加

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -70,6 +70,20 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "email-smtp.ap-northeast-1.amazonaws.com",
+    port: 587,
+    use_name: Rails.application.credentials.dig(:ses, :smtp_user),
+    password: Rails.application.credentials.dig(:ses, :smtp_password),
+    authentication: :plain,
+    enable_starttls_auto: true,
+  }
+  config.action_mailer.default_url_options = {
+    host: "minnaka-map.com",
+    protocol: "https",
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
