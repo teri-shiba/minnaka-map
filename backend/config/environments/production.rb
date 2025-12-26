da5c4513b734165da4aa82451a6dd7b03daaa3c6
teri-shiba <erk.nishimura@gmail.com>
2025-12-26 14:56:32 +0900

    fix: host 変更

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
     enable_starttls_auto: true,
   }
   config.action_mailer.default_url_options = {
-    host: "minnaka-map.com",
+    host: "api.minnaka-map.com",
     protocol: "https",
   }
 
